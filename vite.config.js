2c724c4986377582baa46488d497bf60853c9c65
Aditya Rachman Putra <adityarputra@gmail.com>
2024-10-12 20:56:12 +0700

    add: adjustment to deploy to gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,33 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+import { copyFileSync } from 'fs'
 
+function copyDuckDBFiles() {
+  return {
+    name: 'copy-duckdb-files',
+    writeBundle() {
+      const files = [
+        'duckdb-mvp.wasm',
+        'duckdb-eh.wasm',
+        'duckdb-browser-mvp.worker.js',
+        'duckdb-browser-eh.worker.js'
+      ];
+
+      files.forEach(file => {
+        const srcPath = resolve(__dirname, 'public', file);
+        const destPath = resolve(__dirname, 'dist', file);
+        copyFileSync(srcPath, destPath);
+      });
+    }
+  }
+}
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    copyDuckDBFiles()
+    ],
+  base: '/duck-quanvas/',
   server: {
     headers: {
       "Cross-Origin-Opener-Policy": "same-origin",
